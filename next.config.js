f416bb945eccd03ecb9a66956ed612cbfe4a4936
somosomosomosan <nagekonapps@gmail.com>
2023-07-16 15:47:13 +0900

    pyo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	output: 'export',
+	reactStrictMode: true,
+	// Optional: Add a trailing slash to all paths `/about` -> `/about/`
+	// trailingSlash: true,
+	// Optional: Change the output directory `out` -> `dist`
+	// distDir: 'dist',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
