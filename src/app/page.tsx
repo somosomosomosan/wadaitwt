cec6e9c6850448aae1a6ec5720307c40832fe7c1
somosomosomosan <nagekonapps@gmail.com>
2023-07-21 03:19:55 +0900

    back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,12 @@
 'use client';
 
 import * as React from 'react';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import Link from 'next/link';
-import { Button, ButtonGroup } from '@chakra-ui/react';
-const colors = {
-	brand: {
-		900: '#1a365d',
-		800: '#153e75',
-		700: '#2a69ac',
-	},
-};
-const theme = extendTheme({ colors });
 
 export default function Home() {
 	return (
-		<ChakraProvider theme={theme}>
-			<div>
-				<Link href='./smash'>スマブラ</Link>
-				<Button colorScheme='blue'>Button</Button>
-			</div>
-		</ChakraProvider>
+		<div>
+			<Link href='./smash'>スマブラ</Link>
+		</div>
 	);
 }
